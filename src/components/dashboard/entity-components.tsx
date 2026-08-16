8f54172cf3e89c5770564140918eda514758baae
yash27007 <99220041035@klu.ac.in>
2026-08-16 19:48:36 +0000

    fix: center EntityContainer content (mx-auto typo was max-auto)

--- src/components/dashboard/entity-components.tsx
+++ src/components/dashboard/entity-components.tsx
@@ -104,7 +104,7 @@ export const EntityContainer = ({
 }: EntityContainerProps) => {
   return (
     <div className="p-4 md:px-10 md:py-6 h-full">
-      <div className="max-auto max-w-7xl w-full flex flex-col gap-y-8 h-full">
+      <div className="mx-auto max-w-7xl w-full flex flex-col gap-y-8 h-full">
         {header}
 
         <div className="flex flex-col gap-y-4 h-full">
